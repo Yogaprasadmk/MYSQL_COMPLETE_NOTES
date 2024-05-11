3aa13acde856dc4e139759f1b0b0fcfda4db19e5
Yogaprasad  M K <120255515+Yogaprasadmk@users.noreply.github.com>
2024-05-11 21:06:40 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,12 @@
+-- Show Databases syntax:SHOW DATABASES 
+SHOW DATABASES;
+-- Drop Database syntax:DROP DATABASE DATABASENAME
+DROP DATABASE;
+-- Use Database Syntax: USE Databasename
+USE yoga;
+-- Create Database Syntax: CREATE DATABASE DATABASENAME
+CREATE DATABASE yogaprasad;
+-- DROP Database Syntax: DROP DATABASE DATABASENAME
+DROP DATABASE yoga;
+-- Select Database Syntax: SELECT DATABASE
+SELECT DATABASE();
